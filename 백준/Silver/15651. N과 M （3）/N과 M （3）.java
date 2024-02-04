ccba25a6f21b09b4aae6135f25082954705dff59
yebeen <83651122+dpqls0356@users.noreply.github.com>
2024-02-05 01:45:54 +0900

    [Silver III] Title: N과 M (3), Time: 588 ms, Memory: 209108 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -0,0 +1,41 @@
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N;
+    static int M;
+    static int cnt = 0;
+    static StringBuilder sb = new StringBuilder();
+    static int arr[];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // N = Integer.parseInt(st.nextToken());
+        // M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[M];
+        re(cnt);
+        System.out.println(sb.toString());
+    }
+
+    public static void re(int cnt) {
+        if (cnt == M) {
+            for (int num : arr) {
+                sb.append(num + " ");
+            }
+            sb.append("\n");
+        } else {
+            for (int i = 1; i <= N; i++) {
+                arr[cnt] = i;
+                re(cnt + 1);
+            }
+        }
+    }
+}
