0fe4f88981c1339a1ef81af204196aa0d436b22b
yebeen <83651122+dpqls0356@users.noreply.github.com>
2024-02-07 16:04:37 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 137 ms, Memory: 21,776 KB -BaekjoonHub

---
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -0,0 +1,31 @@
+import java.util.*;
+import java.io.*;
+
+public class Solution {
+	public static void recursive(int arr[],int cnt) {
+		Arrays.sort(arr);
+		if(cnt-1<0||arr[arr.length-1]-arr[0]<1) {
+			System.out.println((arr[99]-arr[0]));
+		}
+		else {
+			arr[0]++;
+			arr[arr.length-1]--;
+			recursive(arr,--cnt);
+		}
+	}
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int arr[] = new int[100];
+		for(int i=0;i<10;i++) {
+			st = new StringTokenizer(br.readLine());
+			int cnt = Integer.parseInt(st.nextToken());
+			st = new StringTokenizer(br.readLine());
+			for(int k=0;k<100;k++) {
+				arr[k] = Integer.parseInt(st.nextToken());
+			}
+			System.out.print("#"+(i+1)+" ");
+			recursive(arr,cnt);
+		}
+	}
+}
