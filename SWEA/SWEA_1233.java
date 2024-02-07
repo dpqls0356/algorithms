b1a0651665de493e5e4751150ea7e56c9d2ff440
dpqls0356 <dpqls0356@gmail.com>
2024-02-07 23:50:59 +0900

    집에서 푼 문제들

---
+++ SWEA/SWEA_1233.java
@@ -0,0 +1,34 @@
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class SWEA_1233 {
+
+    public static void main(String[] args) throws IOException {
+
+        String operators = "+-/*";
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int t = 0; t < 10; t++) {
+            int res = 1;
+            int n = Integer.parseInt(br.readLine());
+            int leaf = n / 2 + 1; // 첫 단말노드
+            for (int i = 0; i < n; i++) {
+                String[] input = (br.readLine()).split(" ");
+                int root = Integer.parseInt(input[0]);
+                String operator = input[1];
+
+                // 단말노드인 경우엔 연산자를 갖을 수 없다는 성질 사용
+                if (root >= leaf) {
+                    if (operators.contains(operator)) {
+                        res = 0;
+                        continue;
+                    }
+                }
+            }
+            System.out.printf("#%d %d", t + 1, res);
+            System.out.println();
+        }
+    }
+}
