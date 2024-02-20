162d1684a672f0f4327770c932f768c5d7d91c70
yebeen <83651122+dpqls0356@users.noreply.github.com>
2024-02-20 16:43:48 +0900

    [Gold V] Title: 적록색약, Time: 160 ms, Memory: 16428 KB -BaekjoonHub

--- 백준/Gold/10026. 적록색약/적록색약.java
+++ 백준/Gold/10026. 적록색약/적록색약.java
@@ -1,24 +1,25 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int cntR=0;
-	static int cntN=0;
-	static boolean visitedR[][];
-	static boolean visitedN[][];
+	static boolean visited[][] ;
 	static char arr[][];
 	static int x[] = {1,0,-1,0};
 	static int y[] = {0,1,0,-1};
 	static int N;
+	static int red;
+	static int blue;
+	static int green;
+	static int rg;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		arr = new char[N][N];
-		visitedR = new boolean[N][N];
-		visitedN= new boolean[N][N];
+		visited = new boolean[N][N];
 		for(int i=0;i<N;i++) {
 			String str = br.readLine();
 			for(int k=0;k<N;k++) {
@@ -27,74 +28,52 @@ public class Main {
 		}
 		for(int i=0;i<N;i++) {
 			for(int k=0;k<N;k++) {
-				if(!visitedR[i][k]&&arr[i][k]=='R') {
-					visitedR[i][k] = true;
-					DFSR('R',i,k);
-					cntR++;
+				if(!visited[i][k]) {
+					if(arr[i][k]=='R') {
+						DFS('R',i,k);
+						red++;
+					}
+					if(arr[i][k]=='G') {
+						DFS('G',i,k);
+						green++;
+					}
+					if(arr[i][k]=='B') {
+						DFS('B',i,k);
+						blue++;
+					}
 				}
 			}
 		}
 		for(int i=0;i<N;i++) {
-			for(int k=0;k<N;k++) {
-				if(!visitedR[i][k]&&arr[i][k]=='G') {
-					visitedR[i][k] = true;
-					DFSR('G',i,k);
-					cntR++;
-				}
-			}
+			for(int k=0;k<N;k++)visited[i][k]=false;
 		}
 		for(int i=0;i<N;i++) {
 			for(int k=0;k<N;k++) {
-				if(!visitedR[i][k]&&arr[i][k]=='B') {
-					visitedR[i][k] = true;
-					DFSR('B',i,k);
-					cntR++;
+				if(!visited[i][k]&&(arr[i][k]=='G'||arr[i][k]=='R')) {
+					DFS(i,k);
+					rg++;
 				}
 			}
 		}
-		for(int i=0;i<N;i++) {
-			for(int k=0;k<N;k++) {
-				if(!visitedN[i][k]&&(arr[i][k]=='R'||arr[i][k]=='G')) {
-					visitedN[i][k] = true;
-					DFSN(arr[i][k],i,k);
-					cntN++;
-				}
-			}
-		}
-		for(int i=0;i<N;i++) {
-			for(int k=0;k<N;k++) {
-				if(!visitedN[i][k]&&arr[i][k]=='B') {
-					visitedN[i][k] = true;
-					DFSN(arr[i][k],i,k);
-					cntN++;
-				}
-			}
-		}
-		System.out.print(cntR+" "+cntN);
+		System.out.println((red+green+blue)+" "+(rg+blue));
 	}
-	public static void DFSR(char c,int row,int col) {
-			for(int i=0;i<4;i++) {
-				int dx = row + x[i];
-				int dy = col+ y[i];
-				if(dx>=0&&dx<N&&dy>=0&&dy<N&&arr[dx][dy]==c&&!visitedR[dx][dy]) {
-					visitedR[dx][dy] = true;
-					DFSR(c,dx,dy);
-				}
-			}
-	}
-	public static void DFSN(char c,int row,int col) {
+	public static void DFS(char color,int row,int col) {
 		for(int i=0;i<4;i++) {
 			int dx = row + x[i];
 			int dy = col+ y[i];
-			if(dx>=0&&dx<N&&dy>=0&&dy<N&&!visitedN[dx][dy]) {
-				if((c=='R'||c=='G')&&(arr[dx][dy]=='R'||arr[dx][dy]=='G')) {
-					visitedN[dx][dy] = true;
-					DFSN(c,dx,dy);		
-				}
-				else if(c=='B'&&arr[dx][dy]=='B'){
-					visitedN[dx][dy] = true;
-					DFSN(c,dx,dy);	
-				}
+			if(dx>=0&&dx<N&&dy>=0&&dy<N&&arr[dx][dy]==color&&!visited[dx][dy]) {
+				visited[dx][dy] = true;
+				DFS(color,dx,dy);
+			}
+		}
+	}
+	public static void DFS(int row,int col) {
+		for(int i=0;i<4;i++) {
+			int dx = row + x[i];
+			int dy = col+ y[i];
+			if(dx>=0&&dx<N&&dy>=0&&dy<N&&(arr[dx][dy]=='R'||arr[dx][dy]=='G')&&!visited[dx][dy]) {
+				visited[dx][dy] = true;
+				DFS(dx,dy);
 			}
 		}
 	}
