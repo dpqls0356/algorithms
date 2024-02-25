bcbbdc6790d760243897ffd7ee999e2fb126e262
yebeen <83651122+dpqls0356@users.noreply.github.com>
2024-02-26 00:55:00 +0900

    [Silver III] Title: 다음 순열, Time: 388 ms, Memory: 18104 KB -BaekjoonHub

--- 백준/Silver/10972. 다음 순열/다음 순열.java
+++ 백준/Silver/10972. 다음 순열/다음 순열.java
@@ -1,48 +1,45 @@
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.Scanner;
 import java.util.StringTokenizer;
 
-public class Main{
-	public static int N;
-	public static int arr[];
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		
-		N  = Integer.parseInt(st.nextToken());
-		arr = new int[N];
-		st = new StringTokenizer(br.readLine());
-		for(int i=0;i<N;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		find();
-	}
-	public static void find() {
-		int top = N-1;
-		// 꼭대기 찾기
-		while(top>0&&arr[top-1]>=arr[top]) {
-			top--;
-		}
-		if(top==0) {
-			System.out.print(-1);
-			System.exit(0);
-		}
-//		System.out.println(top);
-		int change = N-1;
-		//바꾸는 위치 찾기
-		while(arr[top-1]>=arr[change])change--;
-		swap(top-1,change);
-		Arrays.sort(arr,top,N);
-		for(int i=0;i<N;i++) {
-			System.out.print(arr[i]+" ");	
-		}
-	}
-	
-	public static void swap(int top,int change) {
-		int tmp = arr[top];
-		arr[top] = arr[change];
-		arr[change] = tmp;
-	}
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int arr[] = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int top = N - 1;
+        while (top > 0 && arr[top - 1] > arr[top]) {
+            top--;
+        }
+        if (top == 0) {
+            System.out.println(-1);
+            System.exit(0);
+        }
+        int swapIndex = arr.length - 1;
+        // for (int i = arr.length - 1; i > top; i--) {
+        // if (arr[i] < arr[top - 1])
+        // swapIndex = i;
+        // }
+        while (arr[top - 1] > arr[swapIndex]) {
+            swapIndex--;
+        }
+        int tmp = arr[top - 1];
+        arr[top - 1] = arr[swapIndex];
+        arr[swapIndex] = tmp;
+        Arrays.sort(arr, top, arr.length);
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+    }
 }
