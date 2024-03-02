5d3991c206462c348099a6c83ebfed6cef7be9e2
yebeen <83651122+dpqls0356@users.noreply.github.com>
2024-03-02 22:03:29 +0900

    [Unrated] Title: [SW Test 샘플문제] 프로세서 연결하기, Time: 445 ms, Memory: 87,324 KB -BaekjoonHub

--- SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
+++ SWEA/Unrated/1767. ［SW Test 샘플문제］ 프로세서 연결하기/［SW Test 샘플문제］ 프로세서 연결하기.java
@@ -1,149 +1,157 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Solution {
-    static int maxCore;
-    static int cntCore;
-    static int minLen;
+    static int map[][];
     static ArrayList<int[]> core;
+    static int maxCore;
+    static int minLen;
     static int N;
 
     public static void main(String[] args) throws IOException {
         StringBuilder sb = new StringBuilder();
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
-
         for (int t = 1; t <= T; t++) {
-            maxCore = Integer.MIN_VALUE;
-            cntCore = 0;
-            minLen = Integer.MAX_VALUE;
+
             st = new StringTokenizer(br.readLine());
             N = Integer.parseInt(st.nextToken());
-            int[][] arr = new int[N][N];
 
-            // 코어의 위치 저장
+            map = new int[N][N];
             core = new ArrayList<>();
-
-            // 셀정보받기
+            int curCore = 0;
             for (int i = 0; i < N; i++) {
                 st = new StringTokenizer(br.readLine());
                 for (int k = 0; k < N; k++) {
-                    arr[i][k] = Integer.parseInt(st.nextToken());
-                    if (arr[i][k] == 1)
-                        core.add(new int[] { i, k });
+                    map[i][k] = Integer.parseInt(st.nextToken());
+                    if (map[i][k] == 1) {
+                        // 가장자리에 있는 코어는 고려안함
+                        if (i == 0 || i == N - 1 || k == 0 || k == N - 1)
+                            curCore++;
+                        // 가장자리에 있지않는 코어는 탐색이 필요
+                        else
+                            core.add(new int[] { i, k });
+                    }
                 }
             }
-            find(0, arr, 0);
+
+            maxCore = curCore;
+            minLen = Integer.MAX_VALUE;
+            findMin(0, curCore, 0);
+
             sb.append("#" + t + " " + minLen + "\n");
+
         }
-        System.out.print(sb);
+
+        System.out.println(sb);
+
     }
 
-    // cnt = 탐색한 코어 수 , 배열
-    static void find(int cnt, int spot[][], int cntLen) {
-        // 출력
-        int originLen = cntLen;
-        int originSpot[][] = new int[N][N];
-        for (int i = 0; i < N; i++) {
-            System.arraycopy(spot[i], 0, originSpot[i], 0, N);
-        }
-        // System.out.println("----------copy----------");
-        // for (int i = 0; i < N; i++) {
-        // for (int k = 0; k < N; k++) {
-        // System.out.print(originSpot[i][k] + " ");
-        // }
-        // System.out.println();
-        // }
-        // 모든 코어를 다 고려한 경우
+    public static void findMin(int cnt, int curCore, int curLen) {
         if (cnt == core.size()) {
-            // System.out.println("len: " + cntLen + " core" + cntCore);
-            // for (int i = 0; i < N; i++) {
-            // for (int k = 0; k < N; k++) {
-            // System.out.print(spot[i][k] + " ");
+            updateCore(curCore, curLen);
+        } else {
+            // 미리 배열 카피해두기 - 막힌 길이면 나아갈 수 없기때문
+            int copyCurLen = curLen;
+            int copyMap[][] = new int[N][N];
+            for (int i = 0; i < N; i++) {
+                System.arraycopy(map[i], 0, copyMap[i], 0, N);
+            }
+
+            // System.out.println("-------------map-------------");
+            // for (int a = 0; a < N; a++) {
+            // for (int b = 0; b < N; b++) {
+            // System.out.print(copyMap[a][b] + " ");
             // }
             // System.out.println();
             // }
-            if (maxCore == cntCore) {
-                if (minLen > cntLen)
-                    minLen = cntLen;
-            } else if (maxCore < cntCore) {
-                minLen = cntLen;
-                maxCore = cntCore;
-            }
-        } else {
-            int curCore[] = core.get(cnt);
-            int curx = curCore[0];
-            int cury = curCore[1];
-            // System.out.println(curx + " -------- " + cury);
-            // 코어가 가장자리에 있을 때
-            if (curx == 0 || curx == N - 1) {
-                cntCore++;
-                find(cnt + 1, spot, cntLen);
-                cntCore--;
-            } else {
-                // 코어가 가장자리에 있지않을 때
-                for (int i = 0; i < 4; i++) {
-                    boolean ch = true;
-                    if (i == 0) {
-                        for (int k = curx - 1; k >= 0; k--) {
-                            if (spot[k][cury] == 0) {
-                                cntLen++;
-                                spot[k][cury] = 2;
-                            } else {
-                                ch = false;
-                                break;
-                            }
-                        }
-                    } else if (i == 1) {
-                        for (int k = curx + 1; k < N; k++) {
-                            if (spot[k][cury] == 0) {
-                                cntLen++;
-                                spot[k][cury] = 2;
-                            } else {
-                                ch = false;
-                                break;
-                            }
-                        }
-                    } else if (i == 2) {
-                        for (int k = cury - 1; k >= 0; k--) {
-                            if (spot[curx][k] == 0) {
-                                cntLen++;
-                                spot[curx][k] = 2;
-                            } else {
-                                ch = false;
-                                break;
-                            }
-                        }
-                    } else if (i == 3) {
-                        for (int k = cury + 1; k < N; k++) {
-                            if (spot[curx][k] == 0) {
-                                cntLen++;
-                                spot[curx][k] = 2;
-                            } else {
-                                ch = false;
-                                break;
-                            }
+
+            int cur[] = core.get(cnt);
+            int x = cur[0];
+            int y = cur[1];
+            // 사방탐색하기
+            // 막혀있으면 코어 연결안되고 len도 늘릴 수 없다
+            // 막혀있지않으면 코어 연결하고 len도 늘릴 수 있다.
+            for (int i = 0; i < 4; i++) {
+                boolean isOk = true;
+                // 위
+                if (i == 0) {
+                    for (int k = x - 1; k >= 0; k--) {
+                        if (map[k][y] == 0) {
+                            map[k][y] = 2;
+                            curLen++;
+                        } else {
+                            isOk = false;
+                            break;
                         }
                     }
-                    if (ch) {
-                        cntCore++;
-                        find(cnt + 1, spot, cntLen);
-                        cntCore--;
-                    }  else if(maxCore<=core.size()-cnt+cntCore) {
-                        	 find(cnt + 1, originSpot, originLen);
-                         }
-                    for (int a = 0; a < N; a++) {
-                        System.arraycopy(originSpot[a], 0, spot[a], 0, N);
+                }
+                // 아래
+                if (i == 1) {
+                    for (int k = x + 1; k < N; k++) {
+                        if (map[k][y] == 0) {
+                            map[k][y] = 2;
+                            curLen++;
+                        } else {
+                            isOk = false;
+                            break;
+                        }
                     }
-                    cntLen = originLen;
+                }
+                // 왼쪽
+                if (i == 2) {
+                    for (int k = y - 1; k >= 0; k--) {
+                        if (map[x][k] == 0) {
+                            map[x][k] = 2;
+                            curLen++;
+                        } else {
+                            isOk = false;
+                            break;
+                        }
+                    }
                 }
 
+                // 오른쪽
+                if (i == 3) {
+                    for (int k = y + 1; k < N; k++) {
+                        if (map[x][k] == 0) {
+                            map[x][k] = 2;
+                            curLen++;
+                        } else {
+                            isOk = false;
+                            break;
+                        }
+                    }
+                }
+                if (isOk) {
+                    findMin(cnt + 1, curCore + 1, curLen);
+                } else if (!isOk && maxCore <= curCore + core.size() - cnt) {
+                    for (int k = 0; k < N; k++) {
+                        System.arraycopy(copyMap[k], 0, map[k], 0, N);
+                    }
+                    findMin(cnt + 1, curCore, copyCurLen);
+                }
+                curLen = copyCurLen;
+                for (int k = 0; k < N; k++) {
+                    System.arraycopy(copyMap[k], 0, map[k], 0, N);
+                }
             }
+        }
+    }
+
+    public static void updateCore(int curCore, int curLen) {
+        // 코어가 같을 경우 작은 전선 수로 업데이트
+        // 코어가 다를 경우 무조건 많은 코어의 전선 수로 업데이트
+        // System.out.println("maxCore:" + maxCore + " curCore:" + curCore + " minLen:"
+        // + minLen + " curLen:" + curLen);
+        if (curCore == maxCore) {
+            if (minLen > curLen) {
+                minLen = curLen;
+            }
+        } else if (curCore > maxCore) {
+            minLen = curLen;
+            maxCore = curCore;
         }
     }
 }
