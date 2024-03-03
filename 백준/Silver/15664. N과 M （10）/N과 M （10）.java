3a4fd0c1f7cac3e771c67d4c2145bd9d0d773c2d
yebeen <83651122+dpqls0356@users.noreply.github.com>
2024-03-04 00:15:17 +0900

    [Silver II] Title: N과 M (10), Time: 256 ms, Memory: 18820 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static boolean visitied[];
+    static StringBuilder sb = new StringBuilder();
+    static ArrayList<String> list = new ArrayList<>();
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        M = sc.nextInt();
+        arr = new int[N];
+        visitied = new boolean[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Arrays.sort(arr);
+        res(0, "", 0);
+        System.out.print(sb);
+    }
+
+    public static void res(int cnt, String str, int before) {
+        if (cnt == M) {
+            if (!list.contains(str)) {
+                list.add(str);
+                sb.append(str + "\n");
+            }
+        } else {
+            for (int i = 0; i < N; i++) {
+                if (!visitied[i] && before <= arr[i]) {
+                    visitied[i] = true;
+                    res(cnt + 1, str + arr[i] + " ", arr[i]);
+                    visitied[i] = false;
+                }
+            }
+        }
+    }
+}
