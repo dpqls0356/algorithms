6fefc63a6ac84b036b315487207b2132d7a83391
yebeen <83651122+dpqls0356@users.noreply.github.com>
2024-03-03 18:49:52 +0900

    [Silver III] Title: N과 M (2), Time: 140 ms, Memory: 15780 KB -BaekjoonHub

--- 백준/Silver/15650. N과 M （2）/N과 M （2）.java
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -5,35 +5,46 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N,M;
-	public static void re(int cnt, String str,int beforeNum) {
-		if(cnt>=M) {
-			for(int i=0;i<M;i++) {
-				System.out.print(str.charAt(i)+" ");				
-			}
-			System.out.println();
-		}
-		else {
-			for(int i=beforeNum+1;i<=N;i++) {
-				if(str.contains(Integer.toString(i))) {
-					continue;
-				}
-				re(cnt+1,str+Integer.toString(i),i);
-				str = str.substring(0, str.length());
-			}
-		}
-			
-	}
-	public static void main(String args[]) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		M = Integer.parseInt(st.nextToken());
-		for(int i=1;i<=N;i++) {
-			String str = "";
-			int cnt =1;
-			str = str+Integer.toString(i);
-			re(cnt,str,i);
-		}
-	}
+    static int N;
+    static int M;
+    static int cnt = 0;
+    static StringBuilder sb = new StringBuilder();
+    static boolean visited[];
+    static int arr[];
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        // N = Integer.parseInt(st.nextToken());
+        // M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        visited = new boolean[N];
+        arr = new int[M];
+        re(cnt, 1);
+        System.out.println(sb.toString());
+    }
+
+    public static void re(int cnt, int before) {
+        if (cnt == M) {
+            for (int num : arr) {
+                sb.append(num + " ");
+            }
+            sb.append("\n");
+        } else {
+            for (int i = before; i <= N; i++) {
+                if (!visited[i - 1]) {
+                    // System.out.println("test " + cnt + " " + i + " " + !visited[i - 1]);
+                    arr[cnt] = i;
+                    before = i;
+                    visited[i - 1] = true;
+                    re(cnt + 1, before + 1);
+                    visited[i - 1] = false;
+                }
+
+            }
+        }
+    }
 }
