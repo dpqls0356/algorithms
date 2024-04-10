3328acb3a665c562ff3b17319b4a3075b7d129ae
yebeen <83651122+dpqls0356@users.noreply.github.com>
2024-04-10 21:12:26 +0900

    [Gold IV] Title: 배열 돌리기 4, Time: 260 ms, Memory: 39248 KB -BaekjoonHub

--- 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.java
+++ 백준/Gold/17406. 배열 돌리기 4/배열 돌리기 4.java
@@ -1,129 +1,142 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-    public static int minSum = Integer.MAX_VALUE;
-    public static int N;
-    public static int M;
-    public static int R;
-    public static int cnt;
-    public static boolean visited[];
-    public static int calArr[][];
 
-    public static void main(String args[]) throws IOException {
+    static int N, M;
+    static int min = Integer.MAX_VALUE;
+    static int arr[][];
+    static int calSize;
+    static ArrayList<Integer> calList[];
+    static boolean[] visited;
+
+    public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
-        R = Integer.parseInt(st.nextToken());
+        calSize = Integer.parseInt(st.nextToken());
+        arr = new int[N][M];
+        calList = new ArrayList[calSize];
+        for (int i = 0; i < calSize; i++) {
+            calList[i] = new ArrayList<>();
+        }
+        visited = new boolean[calSize];
 
-        // 배열 입력
-        int arr[][] = new int[N][M];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int k = 0; k < M; k++) {
                 arr[i][k] = Integer.parseInt(st.nextToken());
             }
         }
-
-        // 연산 입력
-        calArr = new int[R][3];
-        for (int i = 0; i < R; i++) {
+        for (int i = 0; i < calSize; i++) {
             st = new StringTokenizer(br.readLine());
-            calArr[i][0] = Integer.parseInt(st.nextToken());
-            calArr[i][1] = Integer.parseInt(st.nextToken());
-            calArr[i][2] = Integer.parseInt(st.nextToken());
+            for (int k = 0; k < 3; k++) {
+                calList[i].add(Integer.parseInt(st.nextToken()));
+            }
         }
 
-        visited = new boolean[R];
-        find(0, arr);
-        // System.out.println("Array :" + minSum);
-        System.out.println(minSum);
+        cal(0, arr);
+        System.out.println(min);
     }
 
-    public static void find(int cnt, int arr[][]) {
-        int beforeArr[][] = new int[N][M];
-        for (int i = 0; i < arr.length; i++) {
-            beforeArr[i] = arr[i].clone();
-        }
-        if (cnt == R) {
-            // 모든 케이스를 돈 경우로 배열의 값 최소 판단하기
+    public static void cal(int len, int rotateArr[][]) {
+        if (len == calSize) {
             for (int i = 0; i < N; i++) {
                 int sum = 0;
                 for (int k = 0; k < M; k++) {
-                    sum += arr[i][k];
-                    // System.out.print(arr[i][k] + " ");
+                    sum += rotateArr[i][k];
+                    // System.out.print(rotateArr[i][k] + " ");
                 }
-                if (sum < minSum)
-                    minSum = sum;
                 // System.out.println();
+                if (min > sum)
+                    min = sum;
             }
-            // System.out.println("------------------");
-
+            // System.out.println("--------------");
         } else {
-            // 모든 경우의 수를 돌리는데 이미 방문(연산)한 곳이면 연산 안하기
-            for (int i = 0; i < R; i++) {
+            for (int i = 0; i < calSize; i++) {
                 if (!visited[i]) {
-                    // System.out.println(cnt + " " + i);
-                    rotate(i, arr);
-                    visited[i] = true;
-                    find(cnt + 1, arr);
-                    for (int k = 0; k < beforeArr.length; k++) {
-                        arr[k] = beforeArr[k].clone();
+                    int[][] sendArr = new int[N][M];
+                    for (int a = 0; a < N; a++) {
+                        System.arraycopy(rotateArr[a], 0, sendArr[a], 0, M);
                     }
+                    sendArr = rotate(i, sendArr);
+                    visited[i] = true;
+                    cal(len + 1, sendArr);
                     visited[i] = false;
                 }
             }
-            // 배열돌리고 다음케이스 불리기
         }
     }
 
-    public static void rotate(int i, int arr[][]) {
+    public static int[][] rotate(int calNumber, int changedArr[][]) {
+        int startRow = calList[calNumber].get(0) - calList[calNumber].get(2) - 1;
+        int startCol = calList[calNumber].get(1) - calList[calNumber].get(2) - 1;
+        int endRow = calList[calNumber].get(0) + calList[calNumber].get(2) - 1;
+        int endCol = calList[calNumber].get(1) + calList[calNumber].get(2) - 1;
 
-        // 342
-        int frontx = calArr[i][0] - calArr[i][2] - 1; // 0
-        int fronty = calArr[i][1] - calArr[i][2] - 1; // 1
-        int backx = calArr[i][0] + calArr[i][2] - 1; // 4
-        int backy = calArr[i][1] + calArr[i][2] - 1; // 5
+        int line = (endRow - startRow) / 2;
+        // System.out.println(line + " " + startRow + " " + startCol + " " + endRow + "
+        // " + endCol);
+        for (int i = 0; i < line; i++) {
+            int lastInput = changedArr[startRow + i][endCol - i];
+            for (int a = endCol - i; a > startCol + i; a--) {
+                changedArr[startRow + i][a] = changedArr[startRow + i][a - 1];
+            }
+            // System.out.println("==========1=============");
+            // for (int n = 0; n < N; n++) {
+            // for (int m = 0; m < M; m++) {
+            // System.out.print(changedArr[n][m] + " ");
+            // }
+            // System.out.println();
+            // }
+            ///////////////////////////////
+            for (int a = startRow + i; a < endRow - i; a++) {
+                changedArr[a][startCol + i] = changedArr[a + 1][startCol + i];
+            }
+            // System.out.println("==========2=============");
+            // for (int n = 0; n < N; n++) {
+            // for (int m = 0; m < M; m++) {
+            // System.out.print(changedArr[n][m] + " ");
+            // }
+            // System.out.println();
+            // }
+            ///////////////////////
 
-        int row = backy - fronty;
-        int col = backx - frontx;
-
-        for (int t = 0; t < Math.min(row, col) / 2; t++) {
-            int tmp = arr[frontx + t][backy - t];
-            // 오른쪽 row는 같고 col이 달라짐 끝에서 시작까지
-            for (int k = backy - t; k > fronty + t; k--) {
-                arr[t + frontx][k] = arr[t + frontx][k - 1];
+            for (int a = startCol + i; a < endCol - i; a++) {
+                changedArr[endRow - i][a] = changedArr[endRow - i][a + 1];
             }
-            // 위쪽
-            for (int k = frontx + t; k < backx - t; k++) {
-                // System.out.println(arr[k + 1][fronty + t] + " " + arr[k][fronty + t]);
-                arr[k][fronty + t] = arr[k + 1][fronty + t];
+            // System.out.println("==========3=============");
+            // for (int n = 0; n < N; n++) {
+            // for (int m = 0; m < M; m++) {
+            // System.out.print(changedArr[n][m] + " ");
+            // }
+            // System.out.println();
+            // }
+            //////////////////////////
+            for (int a = endRow - i; a > startRow + i; a--) {
+                changedArr[a][endCol - i] = changedArr[a - 1][endCol - i];
             }
-            // 왼쪽
-            for (int k = fronty + t; k < backy - t; k++) {
-                // System.out.println(arr[backx - t][k] + " " + arr[backx - t][k + 1]);
-                arr[backx - t][k] = arr[backx - t][k + 1];
-            }
-            // 아래쪽
-            for (int k = backx - t; k > frontx + 1 + t; k--) {
-                // System.out.println(arr[k][backy - t] + " " + arr[k - 1][backy - t]);
-                arr[k][backy - t] = arr[k - 1][backy - t];
-            }
-            // System.out.println("-" + (frontx + t + 1) + " " + (backy - t) + " " + tmp);
-            arr[frontx + t + 1][backy - t] = tmp;
+            // System.out.println("==========4=============");
+            // for (int n = 0; n < N; n++) {
+            // for (int m = 0; m < M; m++) {
+            // System.out.print(changedArr[n][m] + " ");
+            // }
+            // System.out.println();
+            // }
+            //////////////////////////
+            changedArr[startRow + i + 1][endCol - i] = lastInput;
+            // System.out.println("==========last=============");
+            // for (int n = 0; n < N; n++) {
+            // for (int m = 0; m < M; m++) {
+            // System.out.print(changedArr[n][m] + " ");
+            // }
+            // System.out.println();
+            // }
         }
+        return changedArr;
+
     }
 }
-// 시작 시작col
-// 시작지점 : calArr[i][0]-calArr[i][2], calArr[i][1] - calArr[i][2] 에서
-// 끝 끝 col
-// calArr[i][0]+calArr[i][2], calArr[i][1] + calArr[i][2]
-// 시작지점에서 끝부분 높이 길이 구하는 공식 (calArr[][0]+calArr[][2])-(calArr[][0]-calArr[][2]+1)
-// 시작지점에서 끝부분 가로 길이 구하는 공식 (calArr[][1] + calArr[][2]) - (calArr[][1] -
-// calArr[][2])
-// 돌리는 방법 오른쪽으로 밀기 위로 밀기 왼쪽으로 밀기 아래로 밀기 [ 위쪽 오른쪽 상단 값을 tmp에 담아야한다...... ]
