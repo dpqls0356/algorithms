dbb5d7f5d3b356e1d8662b5c24ff01b9500b3ba1
yebeen <83651122+dpqls0356@users.noreply.github.com>
2024-04-11 08:55:25 +0900

    [Silver I] Title: 미로 탐색, Time: 136 ms, Memory: 15072 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -1,48 +1,57 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-	//방문배열
-	//연결리스트
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int arr[][] = new int[N+1][M+1];
-		boolean visited[][] = new boolean[N+1][M+1];
-		for(int i=1;i<N+1;i++) {
-			st = new StringTokenizer(br.readLine());
-			String str = st.nextToken();
-			for(int k=1;k<M+1;k++) {
-				arr[i][k] = str.charAt(k-1)-'0';
-			}
+	static int N,M;
+	static int arr[][];
+	static boolean visited[][];
+	static class Spot{
+		int x;
+		int y;
+		int cnt;
+		public Spot() {}
+		public Spot(int x,int y,int cnt) {
+			this.x=x;
+			this.y=y;
+			this.cnt =cnt;
 		}
-		Queue<int[]> que = new LinkedList<>();
-		int x[] = {1,0,-1,0};
-		int y[] = {0,1,0,-1};
-		que.add(new int[] {1,1});
-		visited[1][1]= true;
+	}
+	public static void main(String[] args)throws IOException {
 		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 StringTokenizer st = new StringTokenizer(br.readLine());
+		 N = Integer.parseInt(st.nextToken());
+		 M = Integer.parseInt(st.nextToken());
+		 
+		 arr = new int[N+1][M+1];
+		 visited = new boolean[N+1][M+1];
+		 for(int i=1;i<N+1;i++) {
+			 String str =br.readLine();  
+			 for(int k=1;k<M+1;k++) {
+				 arr[i][k] = str.charAt(k-1)-'0';
+
+			 }
+		 }
+		 BFS();
+	}
+	public static void BFS() {
+		Queue<Spot> que = new ArrayDeque();
+		que.add(new Spot(1,1,1));
 		while(!que.isEmpty()) {
-			int cur[] = que.poll();
+			Spot cur = que.poll();
+			if(cur.x==N&&cur.y==M) {
+				System.out.println(cur.cnt);
+			}
+			int dx[] = {1,-1,0,0};
+			int dy[] = {0,0,1,-1};
 			for(int i=0;i<4;i++) {
-				//이동위치 찍기
-				int dx = cur[0]+x[i];
-				int dy = cur[1]+y[i];
-				if(dx>=0&&dx<N+1&&dy>=0&&dy<M+1&&!visited[dx][dy]&&arr[dx][dy]!=0) {
-//					System.out.println(arr[dx][dy]+" "+arr[cur[0]][cur[1]]);
-					arr[dx][dy]+=arr[cur[0]][cur[1]];
-					visited[dx][dy] = true;
-					que.add(new int[] {dx,dy});
-					if(dx==N&&dy==M)break;
+				int x = cur.x+dx[i];
+				int y = cur.y+dy[i];
+				if(x>0&&x<N+1&&y>0&&y<M+1&&!visited[x][y]&&arr[x][y]==1) {
+					visited[x][y] = true;
+					que.add(new Spot(x,y,cur.cnt+1));
 				}
 			}
 		}
-		System.out.println(arr[N][M]);
 	}
 }
