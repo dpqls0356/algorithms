3c166b82fbe3dce50149daff8b83cd009c83d2f7
yebeen <83651122+dpqls0356@users.noreply.github.com>
2024-04-23 16:33:10 +0900

    [Gold IV] Title: 불, Time: 688 ms, Memory: 132488 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -1,94 +1,127 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
-
+/**
+ * 
+ * 불 동서남북이동 벽 통과 불가
+ * 
+ * 상근 동서남북 벽 통과 불가 불이 옮겨진 칸 or 불이 옮겨질 예정인 곳으로 이동 불가 상근이가 있는 칸에 불이 옮겨옴 동시에 다른 칸으로
+ * 이동할 수 있다. => 불을 옮기고 상근이 옮기기...
+ * 
+ * 
+ * 입력값 t 1~100 w,h 1~100
+ * 
+ */
 public class Main {
 
-    static char[][] arr;
-    static int N, M;
-    static boolean[][] visited;
-    static Queue<Spot> queue;
-    static int[] dx = {0, 1, 0, -1};
-    static int[] dy = {1, 0, -1, 0};
+	static char arr[][];
+	static int N, M;
+	static boolean visited[][];
+	//상근이 이동을 담을 큐
+	static Queue<Spot> que;
+	//불 이동을 담을 큐
+	static Queue<Spot> fire;
+	static int fireCnt;
 
-    static class Spot {
-        int x;
-        int y;
-        int type; // 0: 상근이, 1: 불
-        int cnt;
+	static class Spot {
+		int x;
+		int y;
+		int cnt;	//이동횟수
 
-        public Spot(int x, int y, int type, int cnt) {
-            this.x = x;
-            this.y = y;
-            this.type = type;
-            this.cnt = cnt;
-        }
-    }
+		public Spot(int x, int y, int cnt) {
+			this.x = x;
+			this.y = y;
+			this.cnt = cnt;
+		}
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+		public Spot(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+	}
 
-        int T = Integer.parseInt(st.nextToken());
+	public static void main(String[] args) throws IOException {
 
-        for (int t = 0; t < T; t++) {
-            st = new StringTokenizer(br.readLine());
-            M = Integer.parseInt(st.nextToken());
-            N = Integer.parseInt(st.nextToken());
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
 
-            arr = new char[N][M];
-            visited = new boolean[N][M];
-            queue = new ArrayDeque<>();
+		int T = Integer.parseInt(st.nextToken());
 
-            Spot start = null;
+		for (int t = 1; t <= T; t++) {
 
-            for (int i = 0; i < N; i++) {
-                String line = br.readLine();
-                for (int j = 0; j < M; j++) {
-                    arr[i][j] = line.charAt(j);
-                    if (arr[i][j] == '@') {
-                        start = new Spot(i, j, 0, 0);
-                    } else if (arr[i][j] == '*') {
-                        queue.offer(new Spot(i, j, 1, 0));
-                    }
-                }
-            }
+			st = new StringTokenizer(br.readLine());
+			M = Integer.parseInt(st.nextToken());
+			N = Integer.parseInt(st.nextToken());
 
-            queue.offer(start);
-            int result = bfs();
-            if (result == -1) {
-                System.out.println("IMPOSSIBLE");
-            } else {
-                System.out.println(result);
-            }
-        }
-    }
+			arr = new char[N][M];
+			visited = new boolean[N][M];
 
-    public static int bfs() {
-        while (!queue.isEmpty()) {
-            Spot spot = queue.poll();
+			que = new ArrayDeque<>();
+			fire = new ArrayDeque<>();
+			for (int i = 0; i < N; i++) {
+				String str = br.readLine();
+				for (int k = 0; k < M; k++) {
+					arr[i][k] = str.charAt(k);
+					if (arr[i][k] == '@') {
+						que.add(new Spot(i, k, 0));
+						visited[i][k] = true;
+					}
+					if (arr[i][k] == '*') {
+						fire.add(new Spot(i, k, 0));
+					}
+				}
+			}
 
-            if (spot.type == 0 && (spot.x == 0 || spot.x == N - 1 || spot.y == 0 || spot.y == M - 1)) {
-                return spot.cnt + 1;
-            }
+			BFS();
+		}
+	}
 
-            for (int i = 0; i < 4; i++) {
-                int nx = spot.x + dx[i];
-                int ny = spot.y + dy[i];
+	public static void BFS() {
+		while (!que.isEmpty()) {
 
-                if (spot.type == 1) { // 불의 경우
-                    if (nx >= 0 && nx < N && ny >= 0 && ny < M && (arr[nx][ny] == '.' || arr[nx][ny] == '@')) {
-                        arr[nx][ny] = '*';
-                        queue.offer(new Spot(nx, ny, 1, spot.cnt + 1));
-                    }
-                } else { // 상근이의 경우
-                    if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny] && arr[nx][ny] == '.') {
-                        visited[nx][ny] = true;
-                        queue.offer(new Spot(nx, ny, 0, spot.cnt + 1));
-                    }
-                }
-            }
-        }
-        return -1;
-    }
+			int dx[] = { 0, 1, 0, -1 };
+			int dy[] = { 1, 0, -1, 0 };
+			
+			
+			if (!fire.isEmpty() && fire.peek().cnt <= que.peek().cnt) {
+				// 불 옮기기
+				fireCnt = fire.size();
+				for (int i = 0; i < fireCnt; i++) {
+					Spot fireSpot = fire.poll();
+					for (int k = 0; k < 4; k++) {
+						int x = fireSpot.x + dx[k];
+						int y = fireSpot.y + dy[k];
+						// 불이 퍼질 수 있는 곳 * #이 아니면
+						if (x >= 0 && x < N && y >= 0 && y < M) {
+							if (arr[x][y] == '.' || arr[x][y] == '@') {
+								arr[x][y] = '*';
+								fire.add(new Spot(x, y, fireSpot.cnt + 1));
+
+							}
+						}
+					}
+				}
+
+			}
+			
+			Spot cur = que.poll();
+			if (cur.x == 0 || cur.x == N - 1 || cur.y == 0 || cur.y == M - 1) {
+				System.out.println(cur.cnt + 1);
+				return;
+			}
+			for (int i = 0; i < 4; i++) {
+				int x = cur.x + dx[i];
+				int y = cur.y + dy[i];
+				if (x >= 0 && x < N && y >= 0 && y < M && !visited[x][y] && arr[x][y] == '.') {
+					visited[x][y] = true;
+					que.add(new Spot(x, y, cur.cnt + 1));
+				}
+			}
+		}
+		System.out.println("IMPOSSIBLE");
+	}
 }
