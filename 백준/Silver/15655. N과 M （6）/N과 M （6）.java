4e849b9fa4edafba58021b32f3daa3d3a8ad5b91
yebeen <83651122+dpqls0356@users.noreply.github.com>
2024-12-28 12:31:16 +0900

    [Silver III] Title: N과 M (6), Time: 124 ms, Memory: 15856 KB -BaekjoonHub

--- 백준/Silver/15655. N과 M （6）/N과 M （6）.java
+++ 백준/Silver/15655. N과 M （6）/N과 M （6）.java
@@ -1,56 +1,48 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
-
-// sort해서 순열 풀 듯이 하면 풀 수 있다.
+import java.util.*;
+import java.io.*;
 public class Main {
-    static int N;
-    static int M;
-    static int cnt = 0;
-    static StringBuilder sb = new StringBuilder();
-    static boolean visited[];
-    static int arr[];
-    static int inputNum[];
 
-    public static void main(String[] args) throws IOException {
+    static private int size;
+    static private int numberSize;
+    static private int[] numbers;
+    static private int[] arr;
+    static private StringBuilder sb = new StringBuilder();
 
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+    public  static void main(String args[]) throws IOException{
 
-        // N = Integer.parseInt(st.nextToken());
-        // M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        visited = new boolean[N];
-        arr = new int[M];
-        inputNum = new int[N];
-        st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < N; i++) {
-            inputNum[i] = Integer.parseInt(st.nextToken());
+    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    StringTokenizer st = new StringTokenizer(br.readLine());
+
+    numberSize = Integer.parseInt(st.nextToken());
+    size = Integer.parseInt(st.nextToken());
+    arr = new int[size];
+    numbers = new int[numberSize];
+
+    st = new StringTokenizer(br.readLine());
+    for(int i=0;i<numberSize;i++){
+        numbers[i] = Integer.parseInt(st.nextToken());
         }
-        Arrays.sort(inputNum);
-        re(cnt, 0);
-        System.out.println(sb.toString());
-    }
 
-    public static void re(int cnt, int before) {
-        if (cnt == M) {
-            for (int num : arr) {
-                sb.append(num + " ");
+    Arrays.sort(numbers);
+
+    recrusive(0,0);
+
+    System.out.println(sb.toString());
+
+    }
+    public static void recrusive(int depth,int beforeNumber){
+        if(depth==size){
+            for(int i = 0;i<size;i++){
+                sb.append(arr[i]+" ");
             }
             sb.append("\n");
-        } else {
-            for (int i = cnt; i < N; i++) {
-                if (!visited[i] && inputNum[i] > before) {
-                    arr[cnt] = inputNum[i];
-                    visited[i] = true;
-                    re(cnt + 1, inputNum[i]);
-                    visited[i] = false;
-                }
+        }
+        else{
+            for(int i = beforeNumber;i<numberSize;i++){
+                arr[depth] =  numbers[i];
+                recrusive(depth+1,i+1);
             }
         }
     }
+
 }
