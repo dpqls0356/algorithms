84cf167f94f509bc160ee238f7494029f2302c50
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-01-15 11:40:45 +0900

    [Bronze I] Title: 2진수 8진수, Time: 532 ms, Memory: 67476 KB -BaekjoonHub

--- 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
+++ 백준/Bronze/1373. 2진수 8진수/2진수 8진수.java
@@ -16,13 +16,13 @@ public class Main {
         int size = inputNum.length();
         StringBuilder sb = new StringBuilder();
         for(int i=0;i<size/3;i++){
-            int changeSum = 0;
-            for(int k=0;k<3;k++){
-                int n = Integer.parseInt(inputNum.charAt(size-1-i*3-k)+"");
-                int change = (int)Math.pow(2,k)*n;
-                changeSum+=change;
-            }
-            sb.insert(0,changeSum);
+            int changeSum = 
+            (int)
+            (Integer.parseInt(inputNum.charAt(i*3)+"")*Math.pow(2,2)
+            +Integer.parseInt(inputNum.charAt(i*3+1)+"")*Math.pow(2,1)
+            +Integer.parseInt(inputNum.charAt(i*3+2)+"")*Math.pow(2,0));
+            
+            sb.append(changeSum);
         }
         
         System.out.print(sb.toString());
