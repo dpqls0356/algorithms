637935c01b2940fc64f58b564e546e53e8f59bf1
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-02-03 13:07:33 +0900

    [Gold II] Title: 피보나치 수 6, Time: 172 ms, Memory: 17736 KB -BaekjoonHub

--- 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         
         Scanner sc = new Scanner(System.in);
-        // long N = sc.nextLong()%MOD; 피보 4인가 5에선 주기가 있었기에 %MOD를 했지만 지금은 하면 안됨
+
         long N = sc.nextLong();
         
         if(N==0||N==1){
@@ -32,41 +32,30 @@ public class Main {
             return basicArr;
         }
         
-        long[][] resultArr = new long[2][2];
-        
         long[][] smallArr = cal(arr,exponent/2);
         
-        //행렬곱하기 단 계산마다 %mod
+        long[][] resultArr = multiply(smallArr,smallArr);
+
+        if(exponent%2!=0){
+            resultArr = multiply(resultArr,basicArr);
+        }         
+        
+        return resultArr;
+    }
+    
+    public static long[][] multiply(long[][] arr1, long[][] arr2){
+        
+        long[][] resultArr = new long[2][2];
+        
         for(int i=0;i<2;i++){
             for(int k=0;k<2;k++){
                 for(int j=0;j<2;j++){
-                    resultArr[i][k] = (resultArr[i][k]+(smallArr[i][j]*smallArr[j][k])%MOD)%MOD;
+                    resultArr[i][k] = (resultArr[i][k]+(arr1[i][j]*arr2[j][k])%MOD)%MOD;
                 }
             }
         }
         
-        //홀수면 한번 더 곱해야함
-        // if(exponent%2!=0){
-            // for(int i=0;i<2;i++){
-                // for(int k=0;k<2;k++){
-                    // for(int j=0;j<2;j++){
-                        // resultArr[i][k] = (resultArr[i][k]+(basicArr[i][j]*smallArr[j][k])%MOD)%MOD;
-                    // }
-                // }
-            // }
-        // }
-        if(exponent%2!=0){
-            long[][] temp = new long[2][2];  // 임시 배열 사용
-            for(int i=0;i<2;i++){
-                for(int k=0;k<2;k++){
-                    for(int j=0;j<2;j++){
-                        temp[i][k] = (temp[i][k]+(arr[i][j]*resultArr[j][k])%MOD)%MOD;
-                    }
-                }
-            }
-            return temp;  // 임시 배열 반환
-        }         
-        
         return resultArr;
+        
     }
 }
