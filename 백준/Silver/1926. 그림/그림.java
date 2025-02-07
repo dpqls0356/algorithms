2db823a1bb78140b099b2ec794c80e82bf806488
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-02-07 13:27:18 +0900

    [Silver I] Title: 그림, Time: 344 ms, Memory: 52768 KB -BaekjoonHub

--- 백준/Silver/1926. 그림/그림.java
+++ 백준/Silver/1926. 그림/그림.java
@@ -1,70 +1,65 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
+
 
 public class Main {
 
-  static int[][] arr;
-  static boolean[][] visited;
-  static int maxSize = Integer.MIN_VALUE;
-  static int print = 0;
-  static int N, M;
-
-  public static void main(String[] args) throws IOException {
-    BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    StringTokenizer st = new StringTokenizer(br.readLine());
-    N = Integer.parseInt(st.nextToken());
-    M = Integer.parseInt(st.nextToken());
-
-    arr = new int[N][M];
-    visited = new boolean[N][M];
-
-    for (int i = 0; i < N; i++) {
-      st = new StringTokenizer(br.readLine());
-      for (int k = 0; k < M; k++) {
-        arr[i][k] = Integer.parseInt(st.nextToken());
-      }
-    }
-
-    for (int i = 0; i < N; i++) {
-      for (int k = 0; k < M; k++) {
-        if (!visited[i][k] && arr[i][k] == 1 )  {
-          BFS(i, k);
-          print++;
+    private static boolean[][] visited;
+    private static int[][] arrow = {{0,-1},{0,1},{-1,0},{1,0}};
+    private static int maxSize= 0;
+    private static int count = 0;
+    private static int[][] map;
+    private static int row;
+    private static int col;
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        st = new StringTokenizer(br.readLine());
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
+        
+        map = new int[row][col];
+        visited = new boolean[row][col];
+        
+        for(int i=0;i<row;i++){
+            st = new StringTokenizer(br.readLine());
+            for(int k=0;k<col;k++){
+                map[i][k] = Integer.parseInt(st.nextToken());
+            }
         }
-      }
-    }
-    if(print==0)System.out.println("0\n0");
-    else System.out.println(print + "\n" + maxSize);
-
-  }
-
-  public static void BFS(int i, int k) {
-
-    Queue<int[]> que = new ArrayDeque<>();
-    que.add(new int[] { i, k });
-    visited[i][k] = true;
-    int size = 1;
-    while (!que.isEmpty()) {
-      int cur[] = que.poll();
-      int curX = cur[0];
-      int curY = cur[1];
-      int dx[] = { 0, 1, 0, -1 };
-      int dy[] = { 1, 0, -1, 0 };
-      for (int a = 0; a < 4; a++) {
-        int x = curX + dx[a];
-        int y = curY + dy[a];
-        if (x >= 0 && x < N && y >= 0 && y < M && !visited[x][y] && arr[x][y] == 1) {
-          visited[x][y] = true;
-          que.add(new int[] { x, y });
-          size++;
+        
+        for(int i=0;i<row;i++){
+            for(int k=0;k<col;k++){
+                if(!visited[i][k]&&map[i][k]==1){
+                    // System.out.println("start\n"+i+" "+k);
+                    count++;
+                    maxSize = Math.max(countingPrint(i,k),maxSize);
+                }
+            }
         }
-      }
+        System.out.println(count);
+        System.out.println(maxSize);
     }
-    if (maxSize < size)
-      maxSize = size;
-  }
+    
+    public static int countingPrint(int curX,int curY){
+        
+        int size = 1;
+        
+        visited[curX][curY] = true;
+        
+        for(int i=0;i<4;i++){
+            int newX = curX+arrow[i][1];
+            int newY = curY+arrow[i][0];
+            //x,y가 범위를 넘어서거나 0이거나 이미 방문한 곳이면 접근 불가이기에 zeroCnt 를 늘려주기
+            if(newX>=0&&newX<row&&newY>=0&&newY<col&&map[newX][newY]!=0&&!visited[newX][newY]){
+                // System.out.println(newX+" "+newY);
+                size+=countingPrint(newX,newY);
+            }
+        }
+        return size;
+        
+    }
 }
