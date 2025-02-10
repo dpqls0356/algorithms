11302543340ffa6777157cbe222d0eb19589c4b6
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-02-10 10:49:18 +0900

    [Gold V] Title: 토마토, Time: 576 ms, Memory: 129064 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,65 +1,78 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
+/*
+1. 입력을 받을때 익은 토마토 + 빈칸 수 구하기
+2. 입력개수와 1에서 받은 개수가 일치할 경우 0 리턴
+3. bfs()돌리기
+ 
+
+*/
 public class Main {
-
-    static int arr[][];
-    static int N,M;
-    static int notTomato = 0;
-    static Queue<int[]> tomato = new ArrayDeque<>();
-    static boolean visited[][];
+    
+    private static int row;
+    private static int col;
+    private static int[][] map;
+    private static boolean[][] visited;
+    private static int curCnt=0;
+    private static int dx[] = {0,0,-1,1};
+    private static int dy[] = {1,-1,0,0};
+    private static Queue<int[]> que = new LinkedList<>();
+    
     public static void main(String[] args) throws IOException {
-
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        M = Integer.parseInt(st.nextToken());
-        N = Integer.parseInt(st.nextToken());
+        
+        col = Integer.parseInt(st.nextToken());
+        row = Integer.parseInt(st.nextToken());
+        
+        map = new int[row][col];
+        visited = new boolean[row][col];
 
-        arr = new int[N][M];
-        visited = new boolean[N][M];
-
-        for(int i=0;i<N;i++){
+        for(int i=0;i<row;i++){
             st = new StringTokenizer(br.readLine());
-            for(int k=0;k<M;k++){
-                arr[i][k] = Integer.parseInt(st.nextToken());
-                if(arr[i][k]==1){
-                    tomato.add(new int[]{i,k,0});
+            for(int k=0;k<col;k++){
+                map[i][k] = Integer.parseInt(st.nextToken());
+                if(map[i][k]==1){
+                    curCnt++;
                     visited[i][k] = true;
+                    //행 / 열
+                    que.add(new int[]{i,k,0});
                 }
-                else if(arr[i][k]==0)notTomato++;
+                else if(map[i][k]==-1)curCnt++;
+
             }
         }
-        if(notTomato==0){
-            System.out.println("0");
-            return;
+        if(curCnt==row*col){
+            System.out.print(0);
+            System.exit(0);
         }
-        BFS();
+        
+        int result = bfs();
+   
+        System.out.println(result);
     }
-    public static void BFS(){
-        while(!tomato.isEmpty()){
-            int cur[] = tomato.poll();
-            int curX =cur[0];
-            int curY = cur[1];
-
-            int dx[] = {0,1,0,-1};
-            int dy[] = {1,0,-1,0};
-
+    
+    public static int bfs(){
+        
+        while(!que.isEmpty()){
+            int[] cur = que.poll();
             for(int i=0;i<4;i++){
-                int x = curX+dx[i];
-                int y = curY+dy[i];
-                if(x>=0&&x<N&&y>=0&&y<M&&!visited[x][y]&&arr[x][y]==0){
-                    visited[x][y]= true;
-                    arr[x][y] = 1;
-                    tomato.add(new int[]{x,y,cur[2]+1});
-                    notTomato--;
-                }
-                if(notTomato==0){
-                    System.out.println(cur[2]+1);
-                    return;
+                int newY = cur[0]+dy[i]; // 행
+                int newX = cur[1]+dx[i]; // 열
+                if(newX>=0&&newX<col&&newY>=0&&newY<row&&!visited[newY][newX]&&map[newY][newX]==0){
+                    que.add(new int[]{newY,newX,cur[2]+1});
+                    visited[newY][newX] = true;
+                    curCnt++;
+                    if(curCnt==row*col){
+                        return cur[2]+1;
+                    }
                 }
             }
-
         }
-        System.out.println("-1");
+        
+        return -1;
+        
     }
 }
