e358ddce1ac99635584d806bce45250371f9e9da
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-02-18 17:04:43 +0900

    [Silver I] Title: 나이트의 이동, Time: 232 ms, Memory: 71216 KB -BaekjoonHub

--- 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
+++ 백준/Silver/7562. 나이트의 이동/나이트의 이동.java
@@ -1,69 +1,92 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
+/*
+나이트가 이동하려는 하는 칸이 주어지고 몇 번 움직여서 이 칸으로 갈 수 있는지 판단하는 문제
+이동경로는 8개
+x,y
+(-1,2)
+(-2,1)
+(1,2)
+(2,1)
+(-2,-1)
+(-1,-2)
+(1,-2)
+(2,-1)
+
+입력 : 
+테케
+한변의 길이
+현재위치
+가려는위치 
+
+최단 거리 -> bfs
+*/
 
 public class Main {
-    static int dx[] = {-1,-1,-2,-2,1,1,2,2};
-    static int dy[] = {-2,2,-1,1,-2,2,-1,1};
-    static boolean visited[][];
-    static int N;
-    static int startX,startY;
-    static int endX,endY;
-    public static void main(String[] args)throws IOException {
+
+    private static boolean[][] visited;
+    private static int N;
+    private static StringBuilder sb = new StringBuilder();
+    private static int[] dy = {2,  1,  2,  1,  -1,  -2,  -2,  -1};
+    private static int[] dx = {-1, -2,  1,  2,  -2,  -1,  1,  2};
+
+
+    public static void main(String[] args) throws IOException{
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int T = Integer.parseInt(st.nextToken());
-        for(int i=0;i<T;i++){
-
+        StringTokenizer st;
+        
+        int T = Integer.parseInt(br.readLine());
+        
+        while(T>0){
+            
+            N = Integer.parseInt(br.readLine());
+            setVisited(N);
+            
             st = new StringTokenizer(br.readLine());
-            N = Integer.parseInt(st.nextToken());
-
-            visited = new boolean[N][N];
-
+            int startY = Integer.parseInt(st.nextToken());
+            int startX = Integer.parseInt(st.nextToken());    
+            
             st = new StringTokenizer(br.readLine());
-            startX = Integer.parseInt(st.nextToken());
-            startY = Integer.parseInt(st.nextToken());
-
-            st = new StringTokenizer(br.readLine());
-            endX = Integer.parseInt(st.nextToken());
-            endY = Integer.parseInt(st.nextToken());
-
-            if(startY==endY&&startX==endX){
-                System.out.println("0");
-                continue;
-            }
-
-            BFS();
-
+            int endY = Integer.parseInt(st.nextToken());
+            int endX = Integer.parseInt(st.nextToken());
+            
+            int result = bfs(startY,startX,endY,endX);
+            sb.append(result+"\n");
+            
+            T--;
         }
+        
+        System.out.print(sb.toString());
+        
     }
-
-    public static void BFS(){
-        Queue<int[]> que = new ArrayDeque<>();
-        que.add(new int[] {startX,startY,0});
-        visited[startX][startY] = true;
+    
+    public static void setVisited(int n){
+        visited = new boolean[n][n];
+    }
+    
+    public static int bfs(int startY,int startX,int endY,int endX){
+        
+        Queue<int[]> que = new LinkedList<>();
+        
+        que.add(new int[]{startY,startX,0});
+        visited[startY][startX] = true;
+        
         while(!que.isEmpty()){
             int cur[] = que.poll();
-            int curX = cur[0];
-            int curY = cur[1];
-            if(curX==endX&&curY==endY){
-                System.out.println(cur[2]);
-                return;
+            if(cur[0]==endY&&cur[1]==endX){
+                return cur[2];
             }
-
-            for(int i=0;i<8;i++){
-                int x = curX+dx[i];
-                int y = curY+dy[i];
-                if(x>=0&&x<N&&y>=0&&y<N&&!visited[x][y]){
-                    visited[x][y] = true;
-                    que.add(new int[] { x,y,cur[2]+1});
-                    if(x==endX&&y==endY)visited[x][y]= false;
+            for(int i=0;i<dx.length;i++){
+                int y = cur[0]+dy[i];
+                int x = cur[1]+dx[i];
+                if(x>=0&&x<N&&y>=0&&y<N&&!visited[y][x]){
+                    visited[y][x] = true;
+                    que.add(new int[]{y,x,cur[2]+1});
                 }
             }
         }
+        
+        return 0;
     }
 }
