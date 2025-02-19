9a2161e7adc2d5e82f85260f0e5d64cae1ce2dc2
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-02-19 14:02:04 +0900

    [Gold IV] Title: 불, Time: 680 ms, Memory: 94256 KB -BaekjoonHub

--- 백준/Gold/5427. 불/불.java
+++ 백준/Gold/5427. 불/불.java
@@ -1,127 +1,124 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
-/**
- * 
- * 불 동서남북이동 벽 통과 불가
- * 
- * 상근 동서남북 벽 통과 불가 불이 옮겨진 칸 or 불이 옮겨질 예정인 곳으로 이동 불가 상근이가 있는 칸에 불이 옮겨옴 동시에 다른 칸으로
- * 이동할 수 있다. => 불을 옮기고 상근이 옮기기...
- * 
- * 
- * 입력값 t 1~100 w,h 1~100
- * 
- */
+/*
+불은 매초 동서남북으로 퍼짐
+벽에 불이 붙지않음
+상근이는 동서남북 인접한 칸으로 이동하며 1초가 걸림 
+    => 벽 통과 불가
+    => 불이 옮겨진 칸 또는 이제 불이 붙으려는 칸으로 이동 못함 : 불이 먼저 이동한다는 이야기
+얼마나 빨리 탈출하는가 => bfs 최단거리
+
+. 빈공간
+# 벽
+@ 상근위치
+* 불
+
+*/
 public class Main {
+    
+    private static int rangeY;
+    private static int rangeX;
+    private static char[][] map;
+    private static boolean[][] visited;
+    private static int[] dx = {0,0,-1,1};
+    private static int[] dy = {1,-1,0,0};
+    private static StringBuilder sb = new StringBuilder();
+    private static Queue<int[]> sangQue;
+    private static Queue<int[]> fireQue;
+    
+    public static void main(String[] args) throws IOException {
 
-	static char arr[][];
-	static int N, M;
-	static boolean visited[][];
-	//상근이 이동을 담을 큐
-	static Queue<Spot> que;
-	//불 이동을 담을 큐
-	static Queue<Spot> fire;
-	static int fireCnt;
-
-	static class Spot {
-		int x;
-		int y;
-		int cnt;	//이동횟수
-
-		public Spot(int x, int y, int cnt) {
-			this.x = x;
-			this.y = y;
-			this.cnt = cnt;
-		}
-
-		public Spot(int x, int y) {
-			this.x = x;
-			this.y = y;
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
-
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-
-		int T = Integer.parseInt(st.nextToken());
-
-		for (int t = 1; t <= T; t++) {
-
-			st = new StringTokenizer(br.readLine());
-			M = Integer.parseInt(st.nextToken());
-			N = Integer.parseInt(st.nextToken());
-
-			arr = new char[N][M];
-			visited = new boolean[N][M];
-
-			que = new ArrayDeque<>();
-			fire = new ArrayDeque<>();
-			for (int i = 0; i < N; i++) {
-				String str = br.readLine();
-				for (int k = 0; k < M; k++) {
-					arr[i][k] = str.charAt(k);
-					if (arr[i][k] == '@') {
-						que.add(new Spot(i, k, 0));
-						visited[i][k] = true;
-					}
-					if (arr[i][k] == '*') {
-						fire.add(new Spot(i, k, 0));
-					}
-				}
-			}
-
-			BFS();
-		}
-	}
-
-	public static void BFS() {
-		while (!que.isEmpty()) {
-
-			int dx[] = { 0, 1, 0, -1 };
-			int dy[] = { 1, 0, -1, 0 };
-			
-			
-			if (!fire.isEmpty() && fire.peek().cnt <= que.peek().cnt) {
-				// 불 옮기기
-				fireCnt = fire.size();
-				for (int i = 0; i < fireCnt; i++) {
-					Spot fireSpot = fire.poll();
-					for (int k = 0; k < 4; k++) {
-						int x = fireSpot.x + dx[k];
-						int y = fireSpot.y + dy[k];
-						// 불이 퍼질 수 있는 곳 * #이 아니면
-						if (x >= 0 && x < N && y >= 0 && y < M) {
-							if (arr[x][y] == '.' || arr[x][y] == '@') {
-								arr[x][y] = '*';
-								fire.add(new Spot(x, y, fireSpot.cnt + 1));
-
-							}
-						}
-					}
-				}
-
-			}
-			
-			Spot cur = que.poll();
-			if (cur.x == 0 || cur.x == N - 1 || cur.y == 0 || cur.y == M - 1) {
-				System.out.println(cur.cnt + 1);
-				return;
-			}
-			for (int i = 0; i < 4; i++) {
-				int x = cur.x + dx[i];
-				int y = cur.y + dy[i];
-				if (x >= 0 && x < N && y >= 0 && y < M && !visited[x][y] && arr[x][y] == '.') {
-					visited[x][y] = true;
-					que.add(new Spot(x, y, cur.cnt + 1));
-				}
-			}
-		}
-		System.out.println("IMPOSSIBLE");
-	}
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        int T = Integer.parseInt(st.nextToken());
+        
+        while(T>0){
+            
+            st = new StringTokenizer(br.readLine());
+            rangeX = Integer.parseInt(st.nextToken());
+            rangeY = Integer.parseInt(st.nextToken());
+            
+            setArray();
+            
+            for(int i=0;i<rangeY;i++){
+                String str = br.readLine();
+                for(int k=0;k<rangeX;k++){
+                    map[i][k] = str.charAt(k);
+                    if(map[i][k]=='#'){
+                        visited[i][k] = true;
+                    }
+                    else if(map[i][k]=='@'){
+                        sangQue.add(new int[]{i,k,0});
+                        visited[i][k] = true;
+                    }
+                    else if(map[i][k]=='*'){
+                        fireQue.add(new int[]{i,k,0});
+                        visited[i][k] = true;
+                    }
+                }
+            }
+            
+            int result = bfs();
+            if(result==-1){
+                sb.append("IMPOSSIBLE\n");
+            }
+            else sb.append((result+1)+"\n");
+            
+            T--;
+        }
+        
+        System.out.print(sb.toString());
+    }
+    
+    public static void setArray(){
+        visited = new boolean[rangeY][rangeX];
+        map = new char[rangeY][rangeX];
+        sangQue = new LinkedList<>();
+        fireQue = new LinkedList<>();
+    }
+    
+    public static int bfs(){
+        
+        while(!sangQue.isEmpty()){
+            
+            int[] sang = sangQue.poll();
+            if(sang[0]==0||sang[0]==rangeY-1||sang[1]==0||sang[1]==rangeX-1)return sang[2];
+            
+            fireBfs(sang[2]);
+            
+            for(int i=0;i<4;i++){
+                int y = sang[0]+dy[i];
+                int x = sang[1]+dx[i];
+                if(y>=0&&y<rangeY&&x>=0&&x<rangeX&&!visited[y][x]){
+                    sangQue.add(new int[]{y,x,sang[2]+1});
+                    visited[y][x] = true;
+                }
+            }
+        }
+        
+        return -1;
+    }
+    
+    public static void fireBfs(int time){
+        while(!fireQue.isEmpty()){
+            
+            int[] fire = fireQue.poll();
+            
+            if(fire[2]!=time){
+                fireQue.add(fire);
+                return;
+            }
+            
+            for(int i=0;i<4;i++){
+                int y = fire[0]+dy[i];
+                int x = fire[1]+dx[i];
+                if(y>=0&&y<rangeY&&x>=0&&x<rangeX&&!visited[y][x]){
+                    fireQue.add(new int[]{y,x,fire[2]+1});
+                    visited[y][x] = true;
+                }
+            }
+        }
+    }
 }
