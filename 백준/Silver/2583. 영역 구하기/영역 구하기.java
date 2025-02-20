5af0672512f2685cb3ce8d1afaa34ac31d90fe8e
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-02-20 16:27:49 +0900

    [Silver I] Title: 영역 구하기, Time: 112 ms, Memory: 15308 KB -BaekjoonHub

--- 백준/Silver/2583. 영역 구하기/영역 구하기.java
+++ 백준/Silver/2583. 영역 구하기/영역 구하기.java
@@ -1,76 +1,80 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
+/*
+
+분리된 공간과 사이즈 체킹
+row col k(직사각형 갯수 즉 입력값)
+왼쪽 아래 꼭짓점 x(col),y(row) 오른쪽 윗 꼭직점 x(col),y(row)
+
+왼쪽 아래 꼭짓점 0,0 즉 양수만 나옴
+
+사각형 범위를 방문처리해놓고 방문 안한 경우에만 bfs 돌리면 될 듯??
+
+*/
 public class Main {
-
-    static int arr[][];
-    static int N, M;
-    static ArrayList<Integer> list = new ArrayList<>();
-    static int visited[][];
-
+    
+    private static boolean[][] visited;
+    private static ArrayList<Integer> sizeArray = new ArrayList<>();
+    private static int col;
+    private static int row;
+    private static int[] dx = {0,0,-1,1};
+    private static int[] dy = {1,-1,0,0};
+    
     public static void main(String[] args) throws IOException {
-
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
-        int K = Integer.parseInt(st.nextToken());
-        arr = new int[N][M];
-        visited = new int[N][M];
-
-        for (int i = 0; i < K; i++) {
+        
+        row = Integer.parseInt(st.nextToken());
+        col = Integer.parseInt(st.nextToken());
+        int inputSize = Integer.parseInt(st.nextToken());
+        
+        visited = new boolean[row][col];
+        
+        for(int i=0;i<inputSize;i++){
             st = new StringTokenizer(br.readLine());
-            int startY = Integer.parseInt(st.nextToken());
-            int startX = Integer.parseInt(st.nextToken());
-            int endY = Integer.parseInt(st.nextToken());
-            int endX = Integer.parseInt(st.nextToken());
-            for (int k = startX; k <endX; k++) {
-                for (int j = startY; j < endY; j++)
-                    visited[k][j] = 1;
+            int x1 =  Integer.parseInt(st.nextToken());
+            int y1 =  Integer.parseInt(st.nextToken());
+            int x2 =  Integer.parseInt(st.nextToken());
+            int y2 =  Integer.parseInt(st.nextToken());
+            for(int k = x1;k<x2;k++){ // for(int k = x1;k<=x2;k++){ 잘못된 범위 씀
+                for(int j=row-y2;j<row-y1;j++){
+                    visited[j][k] = true; //y,x 반대로 씀
+                }
             }
-
+            
         }
-
-        for (int i = 0; i < N; i++) {
-            for (int k = 0; k < M; k++) {
-                if (visited[i][k]==0) {
-
-                    list.add(BFS(i, k));
+        
+        for(int i=0;i<row;i++){
+            for(int k=0;k<col;k++){
+                if(!visited[i][k]){
+                    int result = dfs(i,k);
+                    sizeArray.add(result);
                 }
             }
         }
-        Collections.sort(list);
-        System.out.println(list.size());
-        for(int i=0;i<list.size();i++) {
-            System.out.print(list.get(i)+" ");
+        
+        Collections.sort(sizeArray);
+        
+        System.out.println(sizeArray.size());
+        for(int i=0;i<sizeArray.size();i++){
+            System.out.print(sizeArray.get(i)+" ");
         }
-
+        
     }
-
-    public static int BFS(int i,int k) {
-
-        int size=0;
-
-        Queue<int[]> que = new ArrayDeque<>();
-        que.add(new int[] {i,k});
-
-        int dx[] = {0,1,0,-1};
-        int dy[] = {1,0,-1,0};
-
-        while(!que.isEmpty()) {
-            int cur[] = que.poll();
-            for(int a=0;a<4;a++) {
-                int x = cur[0]+dx[a];
-                int y = cur[1]+dy[a];
-                if(x>=0&&y>=0&&x<N&&y<M&&visited[x][y]==0) {
-                    size++;
-                    que.add(new int[] {x,y});
-                    visited[x][y] = 1;
-                }
+    
+    public static int dfs(int y,int x){
+        int size = 1;
+        visited[y][x] = true; // 방문체크 안함
+        for(int i=0;i<4;i++){
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            if(newX>=0&&newX<col&&newY>=0&&newY<row&&!visited[newY][newX]){
+                visited[newY][newX] = true;
+                size+=dfs(newY,newX);
             }
         }
-        if(size==0)size=1;
         return size;
     }
 }
