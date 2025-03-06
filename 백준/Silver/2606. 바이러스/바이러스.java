872ba101c93067d6108e49386b19a44cfb03a5f4
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-03-06 20:48:42 +0900

    [Silver III] Title: 바이러스, Time: 104 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Silver/2606. 바이러스/바이러스.java
+++ 백준/Silver/2606. 바이러스/바이러스.java
@@ -1,62 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.StringTokenizer;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	static int N,Edge;
-	static Queue<Integer> que = new LinkedList<>();
-	static ArrayList<Integer> list[] ;
-	static boolean visited[];
-	static int cnt=0;
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		N = Integer.parseInt(st.nextToken());
-		
-		st = new StringTokenizer(br.readLine());
-		Edge =Integer.parseInt(st.nextToken());
-		
-		visited = new boolean[N+1];
-		list = new ArrayList[N+1];
-		
-		for(int i=0;i<N+1;i++) {
-			list[i] = new ArrayList<>();
-		}
-		
-		for(int i=0;i<Edge;i++) {
-			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			list[a].add(b);
-			list[b].add(a);
-		}
-		
-		BFS();
-		
-		System.out.println(cnt);
-		
-	}
-	public static void BFS() {
-		que.add(1);
-		visited[1] = true;
-		
-		while(!que.isEmpty()) {
-			int cur = que.poll();
-			
-			for(int i=0;i<list[cur].size();i++) {
-				
-				if(!visited[list[cur].get(i)]) {
-					visited[list[cur].get(i)] = true;
-					cnt++;
-					que.add(list[cur].get(i));
-				}
-				
-			}
-			
-		}
-	}
+    
+    private static ArrayList<Integer>[] list;
+    private static int node;
+    private static int edge;
+    private static boolean visited[];
+    private static int cnt = 0;
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        
+        node =Integer.parseInt(br.readLine());
+        edge =Integer.parseInt(br.readLine());
+        
+        list = new ArrayList[node+1];
+        visited = new boolean[node+1];
+        
+        for(int i=0;i<=node;i++){
+          list[i] = new ArrayList<>();
+        }
+        
+        for(int i=0;i<edge;i++){
+            st  = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            list[a].add(b);
+            list[b].add(a);
+        }
+        
+        visited[1] = true;
+        dfs(1);
+        System.out.print(cnt);
+    }
+    
+    public static void dfs(int node){
+        for(int i=0;i<list[node].size();i++){
+            if(!visited[list[node].get(i)]){
+                cnt++;
+                visited[list[node].get(i)] = true;
+                dfs(list[node].get(i));
+            }
+        }
+    }
 }
