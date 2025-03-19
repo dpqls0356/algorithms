31c7c9a19ba392bc9de7105556b6a67edca0aef5
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-03-19 18:11:24 +0900

    [Gold I] Title: 벽 부수고 이동하기 3, Time: 4488 ms, Memory: 705956 KB -BaekjoonHub

--- 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
+++ 백준/Gold/16933. 벽 부수고 이동하기 3/벽 부수고 이동하기 3.java
@@ -1,20 +1,6 @@
 import java.io.*;
 import java.util.*;
-/*
-방문배열을 어떤 형태로 할 것인가.
-n * m * k (k=벽을 부순 횟수)
-그러면 낮밤은 ? 꼭 분리해야하는가 ? 
-분리해서 체킹해야할듯 ? n*m*k*2
-왜냐하면 만약 낮에 와서 true처리를 했는데 
-머무르기를 할 때 방문 체크를 하면 낮에 이미 true로 해놨기때문에 머무를수가 없음
-*/
-/*
-실수 리스트 
-1. 방문여부 체크할때 !visited로 안하고 visited로함
-2. Move 생성자에서 x,y로 받아버림 ;;
-3. 큐에 삽입 시 방문 처리 안해줌
 
-*/
 class Move{
     int x;
     int y;
@@ -62,47 +48,47 @@ public class Main {
         
     }
     
-public static int bfs(){
-    Deque<Move> que = new ArrayDeque<>();
-    que.add(new Move(1, 1, 1, 0, 1)); // 낮(1) 시작
-    visited[1][1][0][1] = true;
+    public static int bfs() {
+        Deque<Move> que = new ArrayDeque<>();
+        que.add(new Move(1, 1, 1, 0, 1)); // (y, x, cnt, 부순횟수, 낮=1)
+        visited[1][1][0][1] = true;
 
-    while(!que.isEmpty()){
-        Move cur = que.poll();
+        while (!que.isEmpty()) {
+            Move cur = que.poll();
+            if (cur.x == M && cur.y == N) return cur.cnt;
 
-        if(cur.x == M && cur.y == N) return cur.cnt;
+            int nextDay = cur.isAfternoon == 1 ? 0 : 1;
 
-        int nextDay = cur.isAfternoon == 1 ? 0 : 1;
+            // 상하좌우 탐색
+            for (int i = 0; i < 4; i++) {
+                int y = cur.y + dy[i];
+                int x = cur.x + dx[i];
 
-        // 4방향 탐색
-        for(int i = 0; i < 4; i++){
-            int ny = cur.y + dy[i];
-            int nx = cur.x + dx[i];
+                if (x <= 0 || x > M || y <= 0 || y > N) continue;
 
-            if(ny <= 0 || ny > N || nx <= 0 || nx > M) continue;
-
-            // 벽인 경우
-            if(map[ny][nx] == 1) {
-                // 낮 + 부술 찬스 남아있으면 부수기
-                if(cur.isAfternoon == 1 && cur.useChance < K && !visited[ny][nx][cur.useChance + 1][nextDay]){
-                    visited[ny][nx][cur.useChance + 1][nextDay] = true;
-                    que.add(new Move(ny, nx, cur.cnt + 1, cur.useChance + 1, nextDay));
+                //벽일 경우
+                if(map[y][x] == 1){
+                    if(cur.isAfternoon == 1 && cur.useChance < K && !visited[y][x][cur.useChance + 1][nextDay]){
+                        visited[y][x][cur.useChance + 1][nextDay] = true;
+                        que.add(new Move(y, x, cur.cnt + 1, cur.useChance + 1, nextDay));
+                    }
+                    else if(cur.isAfternoon == 0 && !visited[cur.y][cur.x][cur.useChance][1]){
+                        visited[cur.y][cur.x][cur.useChance][1] = true;
+                        que.add(new Move(cur.y, cur.x, cur.cnt + 1, cur.useChance, 1));
+                    }
                 }
-            }
-            // 길인 경우
-            else if(map[ny][nx] == 0 && !visited[ny][nx][cur.useChance][nextDay]){
-                visited[ny][nx][cur.useChance][nextDay] = true;
-                que.add(new Move(ny, nx, cur.cnt + 1, cur.useChance, nextDay));
-            }
-        }
+                // 길일 경우
+                else if (map[y][x] == 0 && !visited[y][x][cur.useChance][nextDay]) {
+                    visited[y][x][cur.useChance][nextDay] = true;
+                    que.add(new Move(y, x, cur.cnt + 1, cur.useChance, nextDay));
+                }
+                
 
-        // 밤일 때는 "머무르기"를 무조건 고려
-        if(cur.isAfternoon == 0 && !visited[cur.y][cur.x][cur.useChance][nextDay]){
-            visited[cur.y][cur.x][cur.useChance][nextDay] = true;
-            que.add(new Move(cur.y, cur.x, cur.cnt + 1, cur.useChance, nextDay));
+            }
+
+
         }
+        return -1;
     }
-    return -1;
-}
 
 }
