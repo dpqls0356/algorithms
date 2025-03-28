0d5b6dcd617261f08e4cb40f030d02823bfb4bc7
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-03-28 17:57:31 +0900

    [Silver III] Title: 1로 만들기, Time: 92 ms, Memory: 16800 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.java
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -1,40 +1,66 @@
-import java.util.Scanner;
+import java.util.*;
+import java.io.*;
 
 public class Main {
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int n = sc.nextInt();
-		
-		int dp[] = new int[n+1];
-		
-		dp[0] = dp[1] = 0;
-		if(n>1) {
-			dp[2]=1;
-		}
-		if(n>2) {
-			dp[3] = 1;			
-		}
-		int cur = 1;
-		while(cur<=n) {
-			int cnt;
-			int minCnt = Integer.MAX_VALUE;
-			if(cur<4) {
-				cur++;
-				continue;
-			}
-			if(cur%3==0) {
-				cnt = (dp[cur/3]+1);
-				if(minCnt>cnt)minCnt = cnt;
-			}
-			if(cur%2==0) {
-				cnt = (dp[cur/2]+1);
-				if(minCnt>cnt)minCnt = cnt;
-			}
-			cnt = dp[cur-1]+1;
-			if(minCnt>cnt)minCnt = cnt;
-			dp[cur] = minCnt;
-			cur++;
-		}
-		System.out.print(dp[n]);
-	}
+    private static int start;
+    private static int[] cost;
+    private static Queue<int[]> que = new LinkedList<>();
+    
+    public static void main(String[] args) throws IOException {
+        
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        
+        start = Integer.parseInt(st.nextToken());
+        
+        if(start==1){
+            System.out.println(0);
+            return;
+        }
+        
+        que.add(new int[]{start,0});
+        
+        cost = new int[1000001];
+        for(int i=0;i<cost.length;i++){
+            cost[i] = Integer.MAX_VALUE;
+        }
+        
+        cost[start] = 0;
+        
+        while(!que.isEmpty()){
+            int cur[] = que.poll();
+            int pos = cur[0];
+            int cnt = cur[1];
+            if(pos%3==0){
+                if(pos/3==1){
+                    System.out.print(cnt+1);
+                    return ;
+                }
+                if(cost[pos/3]>cnt+1){
+                    que.add(new int[]{pos/3,cnt+1});
+                    cost[pos/3] = cnt+1;
+                }
+            }
+            if(pos%2==0){
+                if(pos/2==1){
+                    System.out.print(cnt+1);
+                    return ;
+                }
+                if(cost[pos/2]>cnt+1){
+                    que.add(new int[]{pos/2,cnt+1});
+                    cost[pos/2] = cnt+1;
+                }
+            }
+            if(pos-1>0){
+                if(pos-1==1){
+                    System.out.print(cnt+1);
+                    return ;
+                }
+                if(cost[pos-1]>cnt+1){
+                    que.add(new int[]{pos-1,cnt+1});
+                    cost[pos-1] = cnt+1;
+                }
+            }
+        }
+    }
 }
