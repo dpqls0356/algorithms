8eb233a09d906e0a46100a12d1f9672caa4929e1
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-04-02 23:08:10 +0900

    [Bronze V] Title: 개수 세기, Time: 180 ms, Memory: 17880 KB -BaekjoonHub

---
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int N = scanner.nextInt();
+        
+        int[] numbers = new int[N];
+        for (int i = 0; i < N; i++) {
+            numbers[i] = scanner.nextInt();
+        }
+        
+        int v = scanner.nextInt();
+        
+        int count = 0;
+        for (int num : numbers) {
+            if (num == v) {
+                count++;
+            }
+        }
+        
+        System.out.println(count);
+        
+        scanner.close();
+    }
+}
+
