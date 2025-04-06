f8076c3006a8fe0f76e600ca8ec22078e479020f
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-04-06 23:10:27 +0900

    [Silver I] Title: RGB거리, Time: 76 ms, Memory: 12052 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.java
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -1,37 +1,37 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
-
+import java.io.*;
+import java.util.*;
+ 
 public class Main {
+ 
+	final static int Red = 0;
+	final static int Green = 1;
+	final static int Blue = 2;
+ 
 	public static void main(String[] args) throws IOException {
+ 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int [][]cost = new int[N][3];
-		for(int i=0;i<N;i++) {
-			st = new StringTokenizer(br.readLine());
-			for(int k=0;k<3;k++) {
-				cost[i][k] = Integer.parseInt(st.nextToken());
-			}
+		StringTokenizer st;
+ 
+		int N = Integer.parseInt(br.readLine());
+ 
+		int[][] Cost = new int[N][3];
+ 
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+ 
+			Cost[i][Red] = Integer.parseInt(st.nextToken());
+			Cost[i][Green] = Integer.parseInt(st.nextToken());
+			Cost[i][Blue] = Integer.parseInt(st.nextToken());
+ 
 		}
-		int [][]sum = new int[N][3];
-		int cur = 0;
-		while(cur<N) {
-			if(cur==0) {
-				for(int i=0;i<3;i++) {
-					sum[cur][i] = cost[cur][i];
-				}
-			}
-			else {
-				for(int i=0;i<3;i++) {
-					if(i==0)sum[cur][i] = Math.min((cost[cur][0]+sum[cur-1][1]),(cost[cur][0]+sum[cur-1][2]));
-					else if(i==1)sum[cur][i] =Math.min((cost[cur][1]+sum[cur-1][0]),(cost[cur][1]+sum[cur-1][2]));
-					else if(i==2)sum[cur][i] =Math.min((cost[cur][2]+sum[cur-1][1]),(cost[cur][2]+sum[cur-1][0]));
-				}
-			}
-			cur++;
+ 
+		for (int i = 1; i < N; i++) {
+			Cost[i][Red] += Math.min(Cost[i - 1][Green], Cost[i - 1][Blue]);
+			Cost[i][Green] += Math.min(Cost[i - 1][Red], Cost[i - 1][Blue]);
+			Cost[i][Blue] += Math.min(Cost[i - 1][Red], Cost[i - 1][Green]);
 		}
-		System.out.println(Math.min(sum[N-1][0], Math.min(sum[N-1][1], sum[N-1][2])));
+ 
+		System.out.println(Math.min(Math.min(Cost[N - 1][Red], Cost[N - 1][Green]), Cost[N - 1][Blue]));
 	}
+ 
 }
