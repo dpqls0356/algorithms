04af5515f3bb39e7d6b92a30f1048e756945c302
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-04-24 13:45:44 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 252 ms, Memory: 21300 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -1,32 +1,28 @@
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-	//시간복잡도 n^2
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		int arr[] = new int[N+1];
-		st = new StringTokenizer(br.readLine());
-		for(int i=1;i<N+1;i++) {
-			arr[i] = Integer.parseInt(st.nextToken());
-		}
-		int lis[] = new int[N+1];
-		for(int i=1;i<N+1;i++) {
-			lis[i] = 1;
-			for(int j=1;j<i;j++) {
-				if(arr[j]<arr[i]&&lis[i]<lis[j]+1) {
-					lis[i] = lis[j]+1;
-				}				
-			}
-		}
-		Arrays.sort(lis);
-		System.out.println(lis[lis.length-1]);
-	}
-	
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n+1];
+        int[] dp = new int[n+1];
+        
+        for(int i=1;i<=n;i++){
+            arr[i] = sc.nextInt();
+            dp[i] = 1;
+        }
+        
+        int maxLen = 1;
+        for(int i=2;i<=n;i++){
+            for(int j=1;j<i;j++){
+                if(arr[i]>arr[j]){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
+                if(maxLen<dp[i])maxLen = dp[i];
+            }
+        }
+        
+        System.out.print(maxLen);
+    }
 }
