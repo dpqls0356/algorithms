46a395932eb0cc6b0e1e4e0fa0dbb605f97fa804
yebeen <83651122+dpqls0356@users.noreply.github.com>
2025-05-17 01:39:05 +0900

    [Bronze V] Title: Who is in the middle?, Time: 168 ms, Memory: 17812 KB -BaekjoonHub

---
+++ 백준/Bronze/6840. Who is in the middle？/Who is in the middle？.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] weights = new int[3];
+        for (int i = 0; i < 3; i++) weights[i] = sc.nextInt();
+        Arrays.sort(weights);
+        System.out.println(weights[1]);
+    }
+}
